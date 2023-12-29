423a1520b3848eadbea21dcad04d5f267bee724a
Saxar88 <v.sasaran@protonmail.com>
2023-12-29 22:23:03 +0200

    build authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-	return <p>Twitch clone</p>;
+	return (
+		<div className="flex flex-col gap-y-4">
+			<h1>Dasboard</h1>
+			<UserButton afterSignOutUrl="/" />
+		</div>
+	);
 }

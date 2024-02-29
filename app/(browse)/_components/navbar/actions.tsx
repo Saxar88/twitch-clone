9ba204140372da4da7b1d9e6f1eb4940a07043a5
Saxar88 <v.sasaran@protonmail.com>
2024-02-29 08:16:29 +0200

    build navbar actions component

---
+++ app/(browse)/_components/navbar/actions.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { currentUser, SignInButton, UserButton } from "@clerk/nextjs";
+import { Clapperboard } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export const Actions = async () => {
+	const user = await currentUser();
+
+	return (
+		<div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
+			{!user && (
+				<SignInButton>
+					<Button>Log in</Button>
+				</SignInButton>
+			)}
+			{!!user && (
+				<div className="flex items-center gap-x-4">
+					<Button
+						size="sm"
+						variant="ghost"
+						className="test-muted-foreground hover:test-primary"
+						asChild>
+						<Link href={`/u/${user.username}`}>
+							<Clapperboard className="h-5 w-5 lg:mr-2" />
+							<span className="hidden lg:block">Dashboard</span>
+						</Link>
+					</Button>
+					<UserButton afterSignOutUrl="/" />
+				</div>
+			)}
+		</div>
+	);
+};
--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { Actions } from "./actions";
 import { Logo } from "./logo";
 import { Search } from "./search";
 
@@ -6,6 +7,7 @@ export const Navbar = () => {
 		<nav className="fixed top-0 flex items-center justify-between w-full h-20 z-[49] px-2 lg:px-4 bg-[#252731] shadow-sm">
 			<Logo />
 			<Search />
+			<Actions />
 		</nav>
 	);
 };
